bb980725cd4a68d1ed633a0815fec2c80513e990
TAETAEHO <xogh9204@gmail.com>
2022-04-26 12:00:00 +0900

    remove router

--- server/router/boards.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const controllers = require("../controllers");
-// const router = express.Router();
-
-// // router.post("/create", controllers.createBoard);
-// // router.get("/getBoard", controllers.findBoard);
-// // router.delete("/delete", controllers.deleteBoard);
-// // router.put("/update", controllers.updateBoard);
